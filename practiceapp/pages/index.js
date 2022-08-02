91b53c1bbfe1287bea91067878af03b075c72da7
bishoe01 <bishoe01@gmail.com>
2022-08-02 23:38:21 +0900

    use router.query to send props to another page

--- practiceapp/pages/index.js
+++ practiceapp/pages/index.js
@@ -1,28 +1,39 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (
-        await fetch(`/api/movies`)).json();
-      
-      setMovies(results);
-    })();
-  }, []);
-  console.log(movies);
+export default function Home({results}) {
+  const router = useRouter();
+  const onClick = (id , title) => {
+    router.push({
+      pathname:`/movies/${id}`,
+      query:{
+        title,
+      },
+    },`/movies/${id}`,
+    );
+  }
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
-        </div>
+      
+      {results?.map((movie) => (
+          <div onClick={()=> onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+          <Link href={{
+            pathname:`/movies/${movie.id}`,
+            query:`/movies/${movie.original_title}`,
+          }} 
+          as= {`/movies/${movie.id}`}>
+          <a>{movie.original_title}</a>
+          </Link>
+        </h4>
+          </div>
       ))}
+      
       <style jsx>{`
         .container {
           display: grid;
@@ -36,6 +47,9 @@ export default function Home() {
           transition: transform 0.2s ease-in-out;
           box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
         }
+        .movie{
+          cursor: pointer;
+        }
         .movie:hover img {
           transform: scale(1.05) translateY(-10px);
         }
@@ -46,4 +60,13 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps(){
+  const {results} = await( await fetch(`http://localhost:3000/api/movies`)).json(); 
+  return{
+    props:{
+      results,
+    }
+  }
 }
