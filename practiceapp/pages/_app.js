04cc24813d2b110b700fd1ce1b1295ac4cc9b061
bishoe01 <bishoe01@gmail.com>
2022-08-02 15:06:56 +0900

    global style jsx

--- practiceapp/pages/_app.js
+++ practiceapp/pages/_app.js
@@ -1,7 +1,19 @@
+import NavBar from '../components/NavBar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <NavBar />
+  <Component {...pageProps} />
+  <style jsx global>{`
+        a{
+          color:black;
+        }
+        `}</style>
+        <footer>@2022_</footer>
+  </>
+  )
 }
 
 export default MyApp
--- practiceapp/pages/index.js
+++ practiceapp/pages/index.js
@@ -7,7 +7,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return(
     <div>
-      <NavBar/>
+      <h1 className='active'>Hello</h1>
     </div>
   )
 }
